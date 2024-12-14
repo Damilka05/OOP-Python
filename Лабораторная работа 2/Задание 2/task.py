040cf52417f54a58c249fb3aa637036936a6b10f
Damilka05 <144436727+Damilka05@users.noreply.github.com>
2024-12-15 00:19:42 +0300

    Загружена первая версия второй работы

--- Лабораторная работа 2/Задание 2/task.py
+++ Лабораторная работа 2/Задание 2/task.py
@@ -8,7 +8,7 @@ BOOKS_DATABASE = [
         "id": 2,
         "name": "test_name_2",
         "pages": 400,
-    },
+    }
 ]
 
 
@@ -22,7 +22,7 @@ class Book:
         return f'Книга "{self.name}"'
 
     def __repr__(self):
-        return f"Book(id_={self.id}, name='{self.name}', pages={self.pages})"
+        return f'Book(id_={self.id}, name=\'{self.name}\', pages={self.pages})'
 
 
 # TODO: написать класс Library
@@ -42,25 +42,17 @@ class Library:
         for i, book in enumerate(self.books):
             if book.id == id_:
                 return i
-        raise ValueError("Книги с запрашиваемым id не существует")
+        raise ValueError('Книги с запрашиваемым id не существует')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     empty_library = Library()  # инициализируем пустую библиотеку
-    
-    # проверяем следующий id для пустой библиотеки
-    print(empty_library.get_next_book_id())
+    print(empty_library.get_next_book_id())  # проверяем следующий id для пустой библиотеки
 
     list_books = [
-        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"])
-        for book_dict in BOOKS_DATABASE
+        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
     ]
-    
-    # инициализируем библиотеку с книгами
-    library_with_books = Library(books=list_books)
-    
-    # проверяем следующий id для непустой библиотеки
-    print(library_with_books.get_next_book_id())
+    library_with_books = Library(books=list_books)  # инициализируем библиотеку с книгами
+    print(library_with_books.get_next_book_id())  # проверяем следующий id для непустой библиотеки
 
-    # проверяем индекс книги с id = 1
-    print(library_with_books.get_index_by_book_id(1))
+    print(library_with_books.get_index_by_book_id(1))  # проверяем индекс книги с id = 1
